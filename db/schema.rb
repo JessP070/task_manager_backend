b43e567541b3551278507e25fdc6288f56cac1de
JessP070 <jcpereira070@gmail.com>
2021-03-09 21:55:56 -0800

    updated schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_030825) do
+ActiveRecord::Schema.define(version: 2021_03_10_055201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,11 @@ ActiveRecord::Schema.define(version: 2021_03_10_030825) do
     t.date "deadline"
     t.string "creator"
     t.boolean "status"
-    t.integer "project_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
+  add_foreign_key "tasks", "projects"
 end
